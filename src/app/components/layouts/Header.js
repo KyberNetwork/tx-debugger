ae13bbff70ba2e92eabeb47272be3435059490d1
trungnx <trung@kyber.network>
2019-08-07 14:32:58 +0700

    Add link to etherscan and enjinx

--- src/app/components/layouts/Header.js
+++ src/app/components/layouts/Header.js
@@ -1,4 +1,5 @@
 import React  from 'react';
+import { ETHERSCAN_URL, ENJINX_URL } from "../../../config/env";
 
 export default function Header(props) {
   return (
@@ -8,7 +9,19 @@ export default function Header(props) {
           <img className={"header__logo"} src={require('../../../assets/images/logos/kyber-network.svg')} alt="Kyber Network"/>
           <div className={"header__content"}>
             <div className={"header__content-title"}>Transaction Hash</div>
-            <div className={"header__content-tx"}>{props.txHash}</div>
+            <div>
+              <span className={"header__content-tx"}>{props.txHash}</span>
+
+              <a href={`${ETHERSCAN_URL}/tx/${props.txHash}`} className={"header__content-link"} title="View on Etherscan" target="_blank" rel="noreferrer noopener">
+                <img src={require('../../../assets/images/icons/etherscan_explorer.svg')} alt="View on Etherscan"/>
+              </a>
+
+              {ENJINX_URL && (
+                <a href={`${ENJINX_URL}/eth/transaction/${props.txHash}`} className={"header__content-link"} title="View on Kyber.Enjinx" target="_blank" rel="noreferrer noopener">
+                  <img src={require('../../../assets/images/icons/kyber_explorer.svg')} alt="View on Kyber.Enjinx"/>
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
