f5340d7c046c371612feadad07123555432fb6be
trungnx <trung@kyber.network>
2019-08-22 10:56:02 +0700

    Update TX Header

--- src/app/components/layouts/Header.js
+++ src/app/components/layouts/Header.js
@@ -1,22 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import env from "../../../config/env";
 
 export default function Header(props) {
   const envConfig = env[props.network];
-  const [isEditing, setIsEditing] = useState(false);
-
-  useEffect(() => {
-    if (isEditing) {
-      props.txInputRef.current.focus();
-    }
-    // eslint-disable-next-line
-  }, [isEditing]);
-
-  function handleEditingTx() {
-    if (props.updateTxHash()) {
-      setIsEditing(false);
-    }
-  }
 
   return (
     <div className={"header"}>
@@ -24,40 +10,32 @@ export default function Header(props) {
         <div className={"header__container"}>
           <img className={"header__logo"} src={require('../../../assets/images/logos/kyber-network.svg')} alt="Kyber Network"/>
           <div className={"header__content"}>
-            <div className={"header__title"}>
-              <span>Transaction Hash</span>
-              <span className={"header__edit-icon"} onClick={() => setIsEditing(!isEditing)}/>
-            </div>
+            <div className={"header__title"}>Transaction Hash</div>
 
-            {!isEditing &&
-              <div className={"header__tx-container"}>
-                <span className={"header__tx"}>{props.txHash}</span>
+            <div className={"header__tx-container common__fade-in"}>
+              <div>
+                <input className={`header__input`} type="text" defaultValue={props.txHash} ref={props.txInputRef}/>
+                <div className={"header__button common__button"} onClick={props.updateTxHash}>Debug</div>
 
-                <a href={`${envConfig.ETHERSCAN_URL}/tx/${props.txHash}`} className={"header__link"}
-                   title="View on Etherscan" target="_blank" rel="noreferrer noopener">
-                  <img src={require('../../../assets/images/icons/etherscan_explorer.svg')} alt="View on Etherscan"/>
-                </a>
-
-                {envConfig.ENJINX_URL && (
-                  <a href={`${envConfig.ENJINX_URL}/eth/transaction/${props.txHash}`} className={"header__link"}
-                     title="View on Kyber.Enjinx" target="_blank" rel="noreferrer noopener">
-                    <img src={require('../../../assets/images/icons/kyber_explorer.svg')} alt="View on Kyber.Enjinx"/>
+                <div className={"header__link-container"}>
+                  <a href={`${envConfig.ETHERSCAN_URL}/tx/${props.txHash}`} className={"header__link"}
+                     title="View on Etherscan" target="_blank" rel="noreferrer noopener">
+                    <img src={require('../../../assets/images/icons/etherscan_explorer.svg')} alt="View on Etherscan"/>
                   </a>
-                )}
-              </div>
-            }
 
-            {isEditing &&
-              <div className={"header__tx-container common__fade-in"}>
-                <div>
-                  <input className={`header__input`} type="text" defaultValue={props.txHash} ref={props.txInputRef}/>
-                  <div className={"header__button common__button"} onClick={handleEditingTx}>Debug</div>
+                  {envConfig.ENJINX_URL && (
+                    <a href={`${envConfig.ENJINX_URL}/eth/transaction/${props.txHash}`} className={"header__link"}
+                       title="View on Kyber.Enjinx" target="_blank" rel="noreferrer noopener">
+                      <img src={require('../../../assets/images/icons/kyber_explorer.svg')} alt="View on Kyber.Enjinx"/>
+                    </a>
+                  )}
                 </div>
+
                 {props.txError &&
                   <div className={"header__error common__error common__fade-in"}>{props.txError}</div>
                 }
               </div>
-            }
+            </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './app/components/App';
 import * as serviceWorker from './serviceWorker';
-import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import { AppProvider } from "./app/reducers";
 
 const routing = (
