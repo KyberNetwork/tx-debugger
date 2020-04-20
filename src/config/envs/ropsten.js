f5841de7a6a3fef21c807dfaff798f5eadd411ad
trungnx <trung@kyber.network>
2020-04-20 16:14:14 +0700

    Replace infura with alchemy node

--- src/config/envs/ropsten.js
+++ src/config/envs/ropsten.js
@@ -1,7 +1,7 @@
 const envConfig = {
   RESERVE_ADDRESS: "0x0fc1cf3e7dd049f7b42e6823164a64f76fc06be0",
   NETWORK_ADDRESS: "0x818e6fecd516ecc3849daf6845e3ec868087b755",
-  NODE_URL: "https://ropsten.infura.io/v3/c1d32946e55e45b78c330954cf543fc7",
+  NODE_URL: "https://eth-ropsten.alchemyapi.io/v2/GvXu_IIrL0U10ZpgTXKjOGIA06KcwzEK",
   ETHERSCAN_URL: "https://ropsten.etherscan.io",
   ENJINX_URL: false
 };
